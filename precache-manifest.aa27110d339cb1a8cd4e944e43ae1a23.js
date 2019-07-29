ee8482177800b5dcac694a967877f717795c1499
Hrishikesh Belgaumkar <hribgm2496@gmail.com>
2019-07-29 17:01:35 +0530

    Updates

---
+++ precache-manifest.aa27110d339cb1a8cd4e944e43ae1a23.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a9514597e6cc91e5a7e1bb6e77c5fa0",
+    "url": "/compare-app-react/index.html"
+  },
+  {
+    "revision": "0045e177693cf2ac149c",
+    "url": "/compare-app-react/static/css/main.3ecf2bd5.chunk.css"
+  },
+  {
+    "revision": "ec17dd9d751765d291bb",
+    "url": "/compare-app-react/static/js/2.609907aa.chunk.js"
+  },
+  {
+    "revision": "0045e177693cf2ac149c",
+    "url": "/compare-app-react/static/js/main.4f33d3ca.chunk.js"
+  },
+  {
+    "revision": "f773c7af0c442e8cd9eb",
+    "url": "/compare-app-react/static/js/runtime~main.9ae55220.js"
+  },
+  {
+    "revision": "89379736c87cd3cc4a3aa9f1abf94e99",
+    "url": "/compare-app-react/static/media/apple.89379736.jpg"
+  },
+  {
+    "revision": "644ed769c6796049282eeae44193ea19",
+    "url": "/compare-app-react/static/media/guava.644ed769.jpg"
+  },
+  {
+    "revision": "298883595690ba906a634ef859a67a94",
+    "url": "/compare-app-react/static/media/orange.29888359.jpg"
+  },
+  {
+    "revision": "2d90baded7f26da5b63c97a156cb36b4",
+    "url": "/compare-app-react/static/media/strawberry.2d90bade.jpg"
+  }
+]);
